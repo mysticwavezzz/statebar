064ef864cb12ba9115b5c265a11bf82715495b25
Mystic Wavez <forniteclanrlx@gmail.com>
2026-08-19 23:39:50 -0400

    Enable CORS and point certify-bar fetch to http://localhost:3000/api/certify-bar

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const config = require('./config.json');
 
 const app = express();
 app.use(express.json());
+
+// Enable CORS for web requests from GitHub Pages or localhost
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 app.use(express.static(__dirname));
 
 let discordClientRef = null;
